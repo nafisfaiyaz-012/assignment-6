9abf07edb5a93187f6a980a4472ea7c17dd4fe17
nafisfaiyaz-012 <contact.nafisfaiyaz@gmail.com>
2025-09-07 04:34:57 +0600

    banner added with mobile responsiveness and all the categories fetched from api and plants by categories fetched also

---
+++ scripts/index.js
@@ -0,0 +1,99 @@
+const categoryContainer = document.getElementById("category-container");
+const cardContainer = document.getElementById("card-container");
+
+const loadCategory = async () => {
+  const url = "https://openapi.programming-hero.com/api/categories";
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayCategory(data.categories);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const displayCategory = (categories) => {
+  categories.forEach((category) => {
+    categoryContainer.innerHTML += `
+    <button onclick="loadPlantByCategory(${category.id})" class="btn w-full bg-transparent mb-1 border-none justify-start font-normal hover:bg-[#15803D] hover:text-white category-btn">${category.category_name}</button>
+    `;
+  });
+};
+
+const loadPlantByCategory = async (categoryId) => {
+    
+    
+    const url = `https://openapi.programming-hero.com/api/category/${categoryId}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+    displayPlantsByCategory(data.plants);
+    
+}
+
+const displayPlantsByCategory = (plantsByCategory) => {
+    cardContainer.innerHTML = '';
+    plantsByCategory.forEach(plant => {
+        cardContainer.innerHTML += `
+        <div class="rounded-xl bg-white overflow-hidden">
+                        <div class="rounded-t-xl">
+                            <img class="h-70 w-full object-cover" src="${plant.image}" alt="">
+                        </div>
+                        <div class="p-3 space-y-3">
+
+                            <h1 class="font-semibold">${plant.name}</h1>
+                            <p>${plant.description}</p>
+                            <div class="flex justify-between">
+                                <p class="px-3 rounded-2xl bg-[#DCFCE7] text-[#15803D]">${plant.category}</p>
+                                <p>ট<span>${plant.price}</span></p>
+                            </div>
+                            <button class="btn w-full bg-[#15803D] rounded-3xl text-white font-normal">Add To
+                                cart</button>
+                        </div>
+                    </div>
+        `
+        
+    })
+    
+}
+
+const LoadAllTree = async () => {
+  const url = "https://openapi.programming-hero.com/api/plants";
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayAllPlants(data.plants);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const displayAllPlants = (Allplants) => {
+    cardContainer.innerHTML = '';
+    Allplants.forEach(plant => {
+        cardContainer.innerHTML += `
+        <div class="rounded-xl bg-white overflow-hidden">
+                        <div class="rounded-t-xl">
+                            <img class="h-70 w-full object-cover" src="${plant.image}" alt="">
+                        </div>
+                        <div class="p-3 space-y-3">
+
+                            <h1 class="font-semibold">${plant.name}</h1>
+                            <p>${plant.description}</p>
+                            <div class="flex justify-between">
+                                <p class="px-3 rounded-2xl bg-[#DCFCE7] text-[#15803D]">${plant.category}</p>
+                                <p>ট<span>${plant.price}</span></p>
+                            </div>
+                            <button class="btn w-full bg-[#15803D] rounded-3xl text-white font-normal">Add To
+                                cart</button>
+                        </div>
+                    </div>
+        `
+        
+    })
+};
+
+loadCategory();
+LoadAllTree();
